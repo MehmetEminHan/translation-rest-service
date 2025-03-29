d440fcf8ee0032f92848d04782532ee86454b1e4
MehmetEminHan <biyikmehmetemin@gmail.com>
2025-03-28 23:59:26 -0400

    ImageRepository.java whitespaces cleaned!

--- src/main/java/com/neuroval/translationApi/repository/ImageRepository.java
+++ src/main/java/com/neuroval/translationApi/repository/ImageRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
-
-
     // Native query to find the recnum for a given typeName
     @Query(value = "SELECT recnum FROM image_type WHERE image_type = :typeName", nativeQuery = true)
     Integer findImageTypeRecnumByTypeName(@Param("typeName") String typeName);
